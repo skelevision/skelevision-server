e71358fc3400e2012117fee9a0fe2f7e403f8f31
Radu Coanda <andrei.coanda@gmail.com>
2019-12-05 13:27:05 +0100

    init

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 
 from flask import Flask, flash, redirect, request, session, url_for, jsonify
 from flask_session import Session
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
+from skelevision import TraceLog
 
 from pm4py.objects.log.importer.xes import factory as xes_import_factory
 
@@ -11,25 +13,32 @@ CACHE_FOLDER = './cache'
 ALLOWED_EXTENSIONS = {'xes', 'gz'}
 
 app = Flask(__name__)
+
+app.config.from_object(__name__)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 1024 ** 3
-SESSION_TYPE = 'filesystem'
 SESSION_FILE_DIR = CACHE_FOLDER
 SESSION_FILE_THRESHOLD = 10
-app.config.from_object(__name__)
+
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+
 Session(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app, supports_credentials=True)
 
 def process(dataset):
     tracelog = dict()
-    log = xes_import_factory.apply(dataset, variant="nonstandard")
+    log = xes_import_factory.apply(dataset)
     for case in log:
         a = tuple([event["concept:name"] for event in case])
 
         if a not in tracelog:
             tracelog[a] = 0
         tracelog[a] += 1
-
-    session["dataset"] = tracelog
+    session["dataset"] = TraceLog(tracelog)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -53,7 +62,8 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             process(filepath)
-            return filepath
+            s_tracelog = [{"key": k, "value": v} for k, v in session['dataset'].items()]
+            return jsonify(s_tracelog)
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -70,4 +80,5 @@ def eval():
     return jsonify(s_tracelog)
 
 if __name__ == "__main_":
-    app.run(debug=True)  # run app in debug mode on port 5000
+    app.debug = True
+    app.run()
